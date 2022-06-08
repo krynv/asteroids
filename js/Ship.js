bbc42bac3dfe259631a32e9afb22c31221c84633
VK <vitaliy.krynytskyy@gmail.com>
2022-06-08 13:40:40 +0100

    Completed the drawing of the spaceship

---
+++ js/Ship.js
@@ -0,0 +1,30 @@
+class Ship {
+    constructor(x, y, r, a) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.a = a;
+    }
+
+    draw(ctx) {
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = this.r / 20;
+
+        ctx.beginPath();
+        ctx.moveTo(
+            this.x + this.r * Math.cos(this.a),
+            this.y - this.r * Math.sin(this.a)
+        ); // nose
+        ctx.lineTo(
+            this.x - this.r * (Math.cos(this.a) + Math.sin(this.a)),
+            this.y + this.r * (Math.sin(this.a) - Math.cos(this.a))
+        ); // rear left
+        ctx.lineTo(
+            this.x - this.r * (Math.cos(this.a) - Math.sin(this.a)),
+            this.y + this.r * (Math.sin(this.a) + Math.cos(this.a))
+        ); // rear right
+        ctx.closePath();
+
+        ctx.stroke();
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 const FPS = 60;
 
+
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+
+const shipWidth = canvas.width / 2;
+const shipHeight = canvas.height / 2;
+const shipSize = 15;
+const shipAngle = 90 / 180 * Math.PI;
+const ship = new Ship(shipWidth, shipHeight, shipSize, shipAngle);
 
 const update = () => {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ship.draw(ctx);
 };
 
 setInterval(update, 1000 / FPS);
