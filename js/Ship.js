d962ec1b21221cacb8a055d103b2b91691a2fed1
VK <vitaliy.krynytskyy@gmail.com>
2022-06-08 13:45:13 +0100

    Added centre of ship

--- js/Ship.js
+++ js/Ship.js
@@ -12,19 +12,26 @@ class Ship {
 
         ctx.beginPath();
         ctx.moveTo(
-            this.x + this.r * Math.cos(this.a),
-            this.y - this.r * Math.sin(this.a)
+            this.x + 4 / 3 * this.r * Math.cos(this.a),
+            this.y - 4 / 3 * this.r * Math.sin(this.a)
         ); // nose
         ctx.lineTo(
-            this.x - this.r * (Math.cos(this.a) + Math.sin(this.a)),
-            this.y + this.r * (Math.sin(this.a) - Math.cos(this.a))
+            this.x - this.r * (2 / 3 * Math.cos(this.a) + Math.sin(this.a)),
+            this.y + this.r * (2 / 3 * Math.sin(this.a) - Math.cos(this.a))
         ); // rear left
         ctx.lineTo(
-            this.x - this.r * (Math.cos(this.a) - Math.sin(this.a)),
-            this.y + this.r * (Math.sin(this.a) + Math.cos(this.a))
+            this.x - this.r * (2 / 3 * Math.cos(this.a) - Math.sin(this.a)),
+            this.y + this.r * (2 / 3 * Math.sin(this.a) + Math.cos(this.a))
         ); // rear right
 
         ctx.closePath(); // cheat and close it without actually telling it where to draw
         ctx.stroke();
+
+        this.drawCentreDot(ctx);
+    }
+
+    drawCentreDot(ctx) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x - 1, this.y - 1, 2, 2);
     }
 }
